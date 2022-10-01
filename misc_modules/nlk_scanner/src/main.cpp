010ec5029332d7422ff34f32c7f602a37a014160
oh3bsg <oh3bsg@gmail.com>
2022-10-01 21:34:13 +0300

    cleaning

--- misc_modules/nlk_scanner/src/main.cpp
+++ misc_modules/nlk_scanner/src/main.cpp
@@ -7,12 +7,8 @@
 #include <spdlog/spdlog.h>
 #include <iostream>
 #include <fstream>
-//#include <sstream>
 #include <config.h>
-//#include <filesystem>
-//#include <json.hpp>
 
-//using nlohmann::json;
 using namespace std;
 json bookmarks;
 std::ofstream scan_log;
@@ -31,9 +27,6 @@ const char* demodModeListTxt = "NFM\0WFM\0AM\0DSB\0USB\0CW\0LSB\0RAW\0";
 #define NUM_OF_TSKIP_FREQ   100
 #define NUM_OF_BANKS        3
 
-//#define SINGLE_
-//#define MULTI_
-
 typedef struct {
     double frq_start;
     double frq_stop;
@@ -50,21 +43,6 @@ public:
 
         loadByName();
         refreshLists();
-
-/*
-        this->search_banks[0].frq_start = 118000000;
-        this->search_banks[0].frq_stop = 137000000;
-        this->search_banks[0].frq_step = 25000;
-        this->search_banks[0].frq_mode = 2; // AM
-        this->search_banks[1].frq_start = 450000000;
-        this->search_banks[1].frq_stop = 470000000;
-        this->search_banks[1].frq_step = 6250;
-        this->search_banks[1].frq_mode = 0; // FM
-        this->search_banks[2].frq_start = 150000000;
-        this->search_banks[2].frq_stop = 180000000;
-        this->search_banks[2].frq_step = 12500;
-        this->search_banks[2].frq_mode = 0; // FM
-*/
     }
 
     ~NLK_ScannerModule() {
@@ -92,78 +70,6 @@ private:
         float menuWidth = ImGui::GetContentRegionAvail().x;
 
         if (_this->running) { ImGui::BeginDisabled(); }
-#ifdef SINGLE_        
-        ImGui::LeftLabel("Start");
-        ImGui::SetNextItemWidth(menuWidth - ImGui::GetCursorPosX());
-        if (ImGui::InputDouble("##start_freq_scanner", &_this->startFreq, 100.0, 100000.0, "%0.0f")) {
-            _this->startFreq = round(_this->startFreq);
-        }
-        ImGui::LeftLabel("Stop");
-        ImGui::SetNextItemWidth(menuWidth - ImGui::GetCursorPosX());
-        if (ImGui::InputDouble("##stop_freq_scanner", &_this->stopFreq, 100.0, 100000.0, "%0.0f")) {
-            _this->stopFreq = round(_this->stopFreq);
-        }
-        ImGui::LeftLabel("Step");
-        ImGui::SetNextItemWidth(menuWidth - ImGui::GetCursorPosX());
-        if (ImGui::InputDouble("##interval_scanner", &_this->interval, 100.0, 100000.0, "%0.0f")) {
-            _this->interval = round(_this->interval);
-        }
-        ImGui::Combo("Mode##scanner_mode", &_this->frq_mode, demodModeListTxt);
-#endif // SINGLE_
-#ifdef MULTI_
-        scanner_banks_ui(_this);
-        ImGui::LeftLabel("Start_1");
-        ImGui::SetNextItemWidth(menuWidth - ImGui::GetCursorPosX());
-        if (ImGui::InputDouble("##start_1_freq_scanner", &_this->search_banks[0].frq_start, 100.0, 100000.0, "%0.0f")) {
-            _this->search_banks[0].frq_start = round(_this->search_banks[0].frq_start);
-        }
-        ImGui::LeftLabel("Stop_1");
-        ImGui::SetNextItemWidth(menuWidth - ImGui::GetCursorPosX());
-        if (ImGui::InputDouble("##stop_1_freq_scanner", &_this->search_banks[0].frq_stop, 100.0, 100000.0, "%0.0f")) {
-            _this->search_banks[0].frq_stop = round(_this->search_banks[0].frq_stop);
-        }
-        ImGui::LeftLabel("Step_1");
-        ImGui::SetNextItemWidth(menuWidth - ImGui::GetCursorPosX());
-        if (ImGui::InputDouble("##interval_1_scanner", &_this->search_banks[0].frq_step, 100.0, 100000.0, "%0.0f")) {
-            _this->search_banks[0].frq_step = round(_this->search_banks[0].frq_step);
-        }
-        ImGui::Combo("Mode##scanner_1_mode", &_this->search_banks[0].frq_mode, demodModeListTxt);
-
-        ImGui::LeftLabel("Start_2");
-        ImGui::SetNextItemWidth(menuWidth - ImGui::GetCursorPosX());
-        if (ImGui::InputDouble("##start_2_freq_scanner", &_this->search_banks[1].frq_start, 100.0, 100000.0, "%0.0f")) {
-            _this->search_banks[1].frq_start = round(_this->search_banks[1].frq_start);
-        }
-        ImGui::LeftLabel("Stop_2");
-        ImGui::SetNextItemWidth(menuWidth - ImGui::GetCursorPosX());
-        if (ImGui::InputDouble("##stop_2_freq_scanner", &_this->search_banks[1].frq_stop, 100.0, 100000.0, "%0.0f")) {
-            _this->search_banks[1].frq_stop = round(_this->search_banks[1].frq_stop);
-        }
-        ImGui::LeftLabel("Step_2");
-        ImGui::SetNextItemWidth(menuWidth - ImGui::GetCursorPosX());
-        if (ImGui::InputDouble("##interval_2_scanner", &_this->search_banks[1].frq_step, 100.0, 100000.0, "%0.0f")) {
-            _this->search_banks[1].frq_step = round(_this->search_banks[1].frq_step);
-        }
-        ImGui::Combo("Mode##scanner_2_mode", &_this->search_banks[1].frq_mode, demodModeListTxt);
-
-        ImGui::LeftLabel("Start_3");
-        ImGui::SetNextItemWidth(menuWidth - ImGui::GetCursorPosX());
-        if (ImGui::InputDouble("##start_3_freq_scanner", &_this->search_banks[2].frq_start, 100.0, 100000.0, "%0.0f")) {
-            _this->search_banks[2].frq_start = round(_this->search_banks[2].frq_start);
-        }
-        ImGui::LeftLabel("Stop_3");
-        ImGui::SetNextItemWidth(menuWidth - ImGui::GetCursorPosX());
-        if (ImGui::InputDouble("##stop_3_freq_scanner", &_this->search_banks[2].frq_stop, 100.0, 100000.0, "%0.0f")) {
-            _this->search_banks[2].frq_stop = round(_this->search_banks[2].frq_stop);
-        }
-        ImGui::LeftLabel("Step_3");
-        ImGui::SetNextItemWidth(menuWidth - ImGui::GetCursorPosX());
-        if (ImGui::InputDouble("##interval_3_scanner", &_this->search_banks[2].frq_step, 100.0, 100000.0, "%0.0f")) {
-            _this->search_banks[2].frq_step = round(_this->search_banks[2].frq_step);
-        }
-        ImGui::Combo("Mode##scanner_3_mode", &_this->search_banks[2].frq_mode, demodModeListTxt);
-#endif // MULTI_
-
         scanner_banks_ui(_this);
         if (_this->running) { ImGui::EndDisabled(); } // ?????
 
@@ -173,12 +79,10 @@ private:
 
         ImGui::LeftLabel("Delay");
         ImGui::SetNextItemWidth(menuWidth - ImGui::GetCursorPosX());
-        //ImGui::SliderFloat("##delay", &_this->delay, 0.0, 500.0);
         ImGui::SliderInt("##delay", &_this->delay, 0, 500);
 
         ImGui::LeftLabel("scan delay");
         ImGui::SetNextItemWidth(menuWidth - ImGui::GetCursorPosX());
-        //ImGui::SliderFloat("##scan_delay", &_this->scan_delay, 100, 5000);
         ImGui::SliderInt("##scan_delay", &_this->scan_delay, 100, 5000);
 
         if (!_this->running) {
@@ -195,98 +99,20 @@ private:
         if (ImGui::Button("T.Skip##scanner_tskip", ImVec2(menuWidth/2,0))) {
             _this->skip_add_temp(_this->frq);
             _this->receiving = false;
-//            bool sql = true;
-//            core::modComManager.callInterface(_this->selectedVFO, 5, &sql, NULL);
         }
         ImGui::SameLine();
         if (ImGui::Button("Skip##scanner_skip", ImVec2(menuWidth/2,0))) {
             
         }
         ImGui::Text(_this->metadata);
-/*
-        //Draw buttons on top of the list
-        ImGui::BeginTable(("scanner_manager_btn_table" + _this->name).c_str(), 3);
-        ImGui::TableNextRow();
-
-        ImGui::TableSetColumnIndex(0);
-        if (ImGui::Button(("Add##_scan_mgr_add_" + _this->name).c_str(), ImVec2(ImGui::GetContentRegionAvail().x, 0))) {
-        }
-
-        ImGui::TableSetColumnIndex(1);
-//        if (selectedNames.size() == 0 && _this->selectedListName != "") { style::beginDisabled(); }
-        if (ImGui::Button(("Remove##_freq_mgr_rem_" + _this->name).c_str(), ImVec2(ImGui::GetContentRegionAvail().x, 0))) {
-            //_this->deleteBookmarksOpen = true;
-        }
-//        if (selectedNames.size() == 0 && _this->selectedListName != "") { style::endDisabled(); }
-
-        ImGui::TableSetColumnIndex(2);
-//        if (selectedNames.size() != 1 && _this->selectedListName != "") { style::beginDisabled(); }
-        if (ImGui::Button(("Edit##_freq_mgr_edt_" + _this->name).c_str(), ImVec2(ImGui::GetContentRegionAvail().x, 0))) {
-
-            _this->editOpen = true;
-            _this->editedBookmark = _this->bookmarks[selectedNames[0]];
-            _this->editedBookmarkName = selectedNames[0];
-            _this->firstEditedBookmarkName = selectedNames[0];
-
-        }
-//        if (selectedNames.size() != 1 && _this->selectedListName != "") { style::endDisabled(); }
-
-        ImGui::EndTable();
-
-        // Search bank list
-        if (ImGui::BeginTable(("scan_manager_bkm_table" + _this->name).c_str(), 2, ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg | ImGuiTableFlags_ScrollY, ImVec2(0, 200))) {
-            ImGui::TableSetupColumn("Enable");
-            ImGui::TableSetupColumn("Name");
-            ImGui::TableSetupScrollFreeze(2, 1);
-            ImGui::TableHeadersRow();
-
-            // dummy list
-            for (int i=0; i<5;i++) {
-                ImGui::TableNextRow();
-                ImGui::TableSetColumnIndex(0);
-                ImGui::Checkbox("", &_this->enabled);
-                ImGui::TableSetColumnIndex(1);
-                ImGui::Text("Name on bank");
-            }
-*/
-/*
-            for (auto& [name, bm] : _this->bookmarks) {
-                ImGui::TableNextRow();
-                ImGui::TableSetColumnIndex(0);
-                ImVec2 min = ImGui::GetCursorPos();
-
-                if (ImGui::Selectable((name + "##_freq_mgr_bkm_name_" + _this->name).c_str(), &bm.selected, ImGuiSelectableFlags_SpanAllColumns | ImGuiSelectableFlags_SelectOnClick)) {
-                    // if shift or control isn't pressed, deselect all others
-                    if (!ImGui::GetIO().KeyShift && !ImGui::GetIO().KeyCtrl) {
-                        for (auto& [_name, _bm] : _this->bookmarks) {
-                            if (name == _name) { continue; }
-                            _bm.selected = false;
-                        }
-                    }
-                }
-                if (ImGui::TableGetHoveredColumn() >= 0 && ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left)) {
-                    applyBookmark(bm, gui::waterfall.selectedVFO);
-                }
-
-                ImGui::TableSetColumnIndex(1);
-                ImGui::Text("%s %s", utils::formatFreq(bm.frequency).c_str(), demodModeList[bm.mode]);
-                ImVec2 max = ImGui::GetCursorPos();
-            }
-*/
-//            ImGui::EndTable();
-//        }
-
     }
 
     void start() {
         if (running) { return; }
-        //current = next_start();
         next_bank(current, stopFreq, interval, frq_mode);
-        //current = startFreq + (gui::waterfall.getViewBandwidth()/2.0);
         running = true;
         receiving = false;
         workerThread = std::thread(&NLK_ScannerModule::worker, this);
-        //stream = sigpath::sinkManager.getStreams(selectedVFO);
         scan_log.open("scan_log.txt", ios::out | ios::app | ios::binary);
         scan_log << "**** Start scanning ****\n";
     }
@@ -365,7 +191,6 @@ private:
                 if ((current + (gui::waterfall.getViewBandwidth()/2.0)) > stopFreq) {
                     // calculate new signalWidth
                     double f = stopFreq-(current - gui::waterfall.getViewBandwidth()/2);
-                    //signalWidth = (f/gui::waterfall.getViewBandwidth()) * signalWidth;
                     signalWidth *= f/gui::waterfall.getViewBandwidth();
                 }
 
@@ -405,7 +230,6 @@ private:
 
                     if ((current - (gui::waterfall.getViewBandwidth()/2.0)) > stopFreq) {
                         next_bank(current, stopFreq, interval, frq_mode);
-                        //current = startFreq + (gui::waterfall.getViewBandwidth()/2.0); 
                     }
                 }
             }
@@ -426,7 +250,6 @@ private:
             if (this->TSkipFrqs[i] == 0) {
                 this->TSkipFrqs[i] = frq;
                 char msg[45];
-                //sprintf(msg, "%d   -  TSkipped\n", frq);
                 sprintf(msg, "%.3lf MHz - TSkipped\n", frq / 1000000.0);
                 scan_log << msg;
                 break;
@@ -444,17 +267,6 @@ private:
         }
     }
 
-/*
-    double next_start(void) {
-        double start_frq;
-        static int i = 0;
-        
-        start_frq = this->search_banks[i++].frq_start + (gui::waterfall.getViewBandwidth()/2.0);
-        if (i > 1) i = 0;
-        return start_frq;
-    }
-*/
-
     void next_bank(double& startF, double& stopF, double& step, int& mode) {
         static int i = 0;
 
@@ -466,7 +278,6 @@ private:
         }
 
         i++;
-        //if (i >= NUM_OF_BANKS) i = 0;
         if (i >= listNames.size()) i = 0;
     }
 
@@ -476,7 +287,6 @@ private:
 
     bool bankEditDialog() {
         bool open = true;
-        //gui::mainWindow.lockWaterfallControls = true;
 
         std::string id = "Edit##freq_manager_edit_popup_"; // + name;
         ImGui::OpenPopup(id.c_str());
@@ -526,10 +336,6 @@ private:
 
             ImGui::EndTable();
 
-            /*
-            bool applyDisabled = (strlen(nameBuf) == 0) || (bookmarks.find(editedBookmarkName) != bookmarks.end() && editedBookmarkName != firstEditedBookmarkName);
-            if (applyDisabled) { style::beginDisabled(); }
-            */
             if (ImGui::Button("Apply")) {
                 open = false;
 
@@ -554,13 +360,10 @@ private:
 
     void refreshLists() {
         listNames.clear();
-        //listNamesTxt = "";
 
         config.acquire();
         for (auto [_name, list] : config.conf["banks"].items()) {
             listNames.push_back(_name);
-            //listNamesTxt += _name;
-            //listNamesTxt += '\0';
         }
         config.release();
     }
@@ -568,16 +371,6 @@ private:
 
     void loadByName(void/*std::string listName*/) {
         banks.clear();
-/*
-        if (std::find(listNames.begin(), listNames.end(), listName) == listNames.end()) {
-            selectedListName = "";
-            selectedListId = 0;
-            loadFirst();
-            return;
-        }
-        selectedListId = std::distance(listNames.begin(), std::find(listNames.begin(), listNames.end(), listName));
-        selectedListName = listName;
-*/
         config.acquire();
         for (auto [name, bank] : config.conf["banks"].items()) {
             search_bank_str _bank;
@@ -599,83 +392,15 @@ private:
             config.conf["banks"][bankName]["step"] = bank.frq_step;
             config.conf["banks"][bankName]["mode"] = bank.frq_mode;
         }
-        //refreshWaterfallBookmarks(false);
         config.release(true);
     }
 
     static void scanner_banks_ui(void* ctx) {
         NLK_ScannerModule* _this = (NLK_ScannerModule*)ctx;
-//        FrequencyManagerModule* _this = (FrequencyManagerModule*)ctx;
         float menuWidth = ImGui::GetContentRegionAvail().x;
 
         // TODO: Replace with something that won't iterate every frame
         std::vector<std::string> selectedNames;
-/*
-        for (auto& [name, bm] : _this->bookmarks) {
-            if (bm.selected) { selectedNames.push_back(name); }
-        }
-
-        float lineHeight = ImGui::GetTextLineHeightWithSpacing();
-
-        float btnSize = ImGui::CalcTextSize("Rename").x + 8;
-        ImGui::SetNextItemWidth(menuWidth - 24 - (2 * lineHeight) - btnSize);
-        if (ImGui::Combo(("##freq_manager_list_sel" + _this->name).c_str(), &_this->selectedListId, _this->listNamesTxt.c_str())) {
-            _this->loadByName(_this->listNames[_this->selectedListId]);
-            config.acquire();
-            config.conf["selectedList"] = _this->selectedListName;
-            config.release(true);
-        }
-        ImGui::SameLine();
-        if (_this->listNames.size() == 0) { style::beginDisabled(); }
-        if (ImGui::Button(("Rename##_freq_mgr_ren_lst_" + _this->name).c_str(), ImVec2(btnSize, 0))) {
-            _this->firstEditedListName = _this->listNames[_this->selectedListId];
-            _this->editedListName = _this->firstEditedListName;
-            _this->renameListOpen = true;
-        }
-        if (_this->listNames.size() == 0) { style::endDisabled(); }
-        ImGui::SameLine();
-        if (ImGui::Button(("+##_freq_mgr_add_lst_" + _this->name).c_str(), ImVec2(lineHeight, 0))) {
-            // Find new unique default name
-            if (std::find(_this->listNames.begin(), _this->listNames.end(), "New List") == _this->listNames.end()) {
-                _this->editedListName = "New List";
-            }
-            else {
-                char buf[64];
-                for (int i = 1; i < 1000; i++) {
-                    sprintf(buf, "New List (%d)", i);
-                    if (std::find(_this->listNames.begin(), _this->listNames.end(), buf) == _this->listNames.end()) { break; }
-                }
-                _this->editedListName = buf;
-            }
-            _this->newListOpen = true;
-        }
-        ImGui::SameLine();
-        if (_this->selectedListName == "") { style::beginDisabled(); }
-        if (ImGui::Button(("-##_freq_mgr_del_lst_" + _this->name).c_str(), ImVec2(lineHeight, 0))) {
-            _this->deleteListOpen = true;
-        }
-        if (_this->selectedListName == "") { style::endDisabled(); }
-
-        // List delete confirmation
-        if (ImGui::GenericDialog(("freq_manager_del_list_confirm" + _this->name).c_str(), _this->deleteListOpen, GENERIC_DIALOG_BUTTONS_YES_NO, [_this]() {
-                ImGui::Text("Deleting list named \"%s\". Are you sure?", _this->selectedListName.c_str());
-            }) == GENERIC_DIALOG_BUTTON_YES) {
-            config.acquire();
-            config.conf["lists"].erase(_this->selectedListName);
-            _this->refreshWaterfallBookmarks(false);
-            config.release(true);
-            _this->refreshLists();
-            _this->selectedListId = std::clamp<int>(_this->selectedListId, 0, _this->listNames.size());
-            if (_this->listNames.size() > 0) {
-                _this->loadByName(_this->listNames[_this->selectedListId]);
-            }
-            else {
-                _this->selectedListName = "";
-            }
-        }
-
-        if (_this->selectedListName == "") { style::beginDisabled(); }
-*/
         //Draw buttons on top of the list
         ImGui::BeginTable(("freq_manager_btn_table"/* + _this->name*/)/*.c_str()*/, 3);
         ImGui::TableNextRow();
@@ -715,7 +440,6 @@ private:
             _this->editOpen = true;
             _this->editedBank = _this->banks[selectedNames[0]];
             _this->editedBankName = selectedNames[0];
-            //_this->firstEditedBookmarkName = selectedNames[0];
         }
         //if (selectedNames.size() != 1 && _this->selectedListName != "") { style::endDisabled(); }
 
